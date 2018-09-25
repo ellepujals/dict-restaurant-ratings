c0a6fb71a1f51f309353ed6e6966858c35154b6a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-24 17:37:41 -0700

    Give the user choices

--- ratings.py
+++ ratings.py
@@ -1,8 +1,33 @@
 import sys
 
+
+def master(the_file):
+    filename = open(the_file)
+
+    while True:
+        choice = input(
+            "Would you like to a) see all the ratings, b) add and rate a " + 
+            "restaurant, or c) quit?: "
+        )
+
+        if "a" == choice:
+            original_dict = list_restaurant_ratings(the_file)
+            print_alpha_ratings(original_dict)
+        elif "b" == choice:
+            custom_input = get_user_input()
+            update_dict = list_restaurant_ratings(the_file)
+            update_dict[custom_input[0]] = custom_input[1]
+            print_alpha_ratings(update_dict)
+        elif "c" == choice:
+            return False
+        else:
+            print("Please enter a, b, or c.")
+
+
+
 def get_user_input():
 
-    new_restaurant_name = input("Choose a restaurant: ")
+    new_restaurant_name = input("Choose a restaurant: ").title()
     new_restaurant_score = input("Rate that restaurant on a scale from 1 to 5: ")
 
     while True:
@@ -17,6 +42,7 @@ def get_user_input():
     new_info = (new_restaurant_name, new_restaurant_score)
 
     return new_info
+
 
 def list_restaurant_ratings(the_file):
 
@@ -34,23 +60,22 @@ def list_restaurant_ratings(the_file):
 
         restaurant_ratings[item[0]] = item[1]
 
-    user_input = get_user_input()
-
-    restaurant_ratings[user_input[0]] = user_input[1]
-
     return restaurant_ratings
 
 
-def print_alpha_ratings(the_file):
+def print_alpha_ratings(the_dict):
 
-    tuples_list_dictionary = list_restaurant_ratings(the_file).items()
+    tuples_list_dictionary = the_dict.items()
     in_order_tuples_list = sorted(tuples_list_dictionary)
 
     for restaurant, rating in in_order_tuples_list:
         print("{} is rated at {}.".format(restaurant, rating))
 
 
-print_alpha_ratings(sys.argv[1])
+master(sys.argv[1])
+
+
+
 
 
 
