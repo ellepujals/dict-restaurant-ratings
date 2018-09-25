1c1abe4b2df05204e1142d96f606a6bebd8f583a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-24 17:49:36 -0700

    Added docstrings

--- ratings.py
+++ ratings.py
@@ -1,7 +1,59 @@
 import sys
 
 
+def get_user_input():
+
+    """Get a restaurant name and score from the user."""
+
+    new_restaurant_name = input("Choose a restaurant: ").title()
+    new_restaurant_score = input("Rate that restaurant on a scale from 1 to 5: ")
+
+    while True:
+
+        if int(new_restaurant_score) > 5 or int(new_restaurant_score) < 1:
+            print("Your rating was out of range. Please choose again.")
+            new_restaurant_score = input("Rate that restaurant on a scale from 1 to 5: ")
+
+        if 1 <= int(new_restaurant_score) <= 5:
+            break
+
+    return (new_restaurant_name, new_restaurant_score)
+
+
+def list_restaurant_ratings(the_file):
+
+    """List restaurant ratings."""
+
+    restaurant_ratings = {}
+    filename = open(the_file)
+    print(filename)
+
+    for line in filename:
+
+        line = line.rstrip()
+
+        item = line.split(":")
+
+        restaurant_ratings[item[0]] = item[1]
+
+    return restaurant_ratings
+
+
+def print_alpha_ratings(the_dict):
+
+    """Print ratings in alphabetical order."""
+
+    tuples_list_dictionary = the_dict.items()
+    in_order_tuples_list = sorted(tuples_list_dictionary)
+
+    for restaurant, rating in in_order_tuples_list:
+        print("{} is rated at {}.".format(restaurant, rating))
+
+
 def master(the_file):
+
+    """Open source file and ask user how to proceed."""
+
     filename = open(the_file)
 
     while True:
@@ -22,54 +74,6 @@ def master(the_file):
             return False
         else:
             print("Please enter a, b, or c.")
-
-
-
-def get_user_input():
-
-    new_restaurant_name = input("Choose a restaurant: ").title()
-    new_restaurant_score = input("Rate that restaurant on a scale from 1 to 5: ")
-
-    while True:
-
-        if int(new_restaurant_score) > 5 or int(new_restaurant_score) < 1:
-            print("Your rating was out of range. Please choose again.")
-            new_restaurant_score = input("Rate that restaurant on a scale from 1 to 5: ")
-
-        if 1 <= int(new_restaurant_score) <= 5:
-            break
-
-    new_info = (new_restaurant_name, new_restaurant_score)
-
-    return new_info
-
-
-def list_restaurant_ratings(the_file):
-
-    """Restaurant rating lister."""
-
-    restaurant_ratings = {}
-    filename = open(the_file)
-    print(filename)
-
-    for line in filename:
-
-        line = line.rstrip()
-
-        item = line.split(":")
-
-        restaurant_ratings[item[0]] = item[1]
-
-    return restaurant_ratings
-
-
-def print_alpha_ratings(the_dict):
-
-    tuples_list_dictionary = the_dict.items()
-    in_order_tuples_list = sorted(tuples_list_dictionary)
-
-    for restaurant, rating in in_order_tuples_list:
-        print("{} is rated at {}.".format(restaurant, rating))
 
 
 master(sys.argv[1])
